3efcb51d9c00638d080c55b4bb562c1cd535169a
LEGENDS-LEGA <user2023arame@gmail.com>
2024-09-07 16:54:20 +0400

    insta arm 2

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+// EmailJS initialization
+emailjs.init("nJj8wW8QPsifD9BA1");
+
+// Հղումը դեպի կոճակը և մուտքագրումը
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 
-emailjs.init({
-  publicKey: "nJj8wW8QPsifD9BA1",
-});
-
+// Երբ կոճակը սեղմվում է, նամակն ուղարկում ենք
 button.addEventListener("click", () => {
   const templateParams = {
     to_name: "me",
     from_name: "me",
     message: input.value,
   };
-  emailjs.send("service_dny056e", "template_hz42nfb", templateParams).then(
-    function (response) {
-      console.log("SUCCESS!", response.status, response.text);
-    },
-    function (error) {
-      console.log("FAILED...", error);
-    }
-  );
+
+  // EmailJS հաղորդագրության ուղարկում
+  emailjs.send("service_dny056e", "template_hz42nfb", templateParams)
+    .then(
+      function(response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function(error) {
+        console.log("FAILED...", error);
+      }
+    );
 });
